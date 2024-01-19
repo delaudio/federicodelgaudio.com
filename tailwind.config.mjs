549e9704c14b143265c0263017b4dca5deded192
Federico Del Gaudio <federico.delgaudio@gmail.com>
2024-01-19 23:34:10 +0100

    Add tailwind and knip config

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [require("@tailwindcss/typography")],
+};
