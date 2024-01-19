4305beadf97f3290cc4a353d81c8f240fbf018d2
Federico Del Gaudio <federico.delgaudio@gmail.com>
2024-01-19 23:35:42 +0100

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "http://localhost:4321",
+  integrations: [
+    tailwind(),
+    icon({
+      iconDir: "src/icons",
+    }),
+    sitemap(),
+  ],
+  image: {
+    domains: ["astro.build"],
+  },
+});
