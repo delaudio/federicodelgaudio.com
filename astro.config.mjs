e3b94019bd9f562ff93780bba50a151b73f9a1e0
Federico Del Gaudio <federico.delgaudio@gmail.com>
2024-01-20 17:25:37 +0100

    Add site url to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
+  site: import.meta.env.SITE_URL,
   integrations: [
     tailwind(),
-    icon({
-      iconDir: "src/icons",
-    }),
     sitemap(),
   ],
   image: {
